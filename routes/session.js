d5d6f43ae49359e3b698836551e3aeee8f538fd3
mac.grewal <mac.grewal@digital.hmrc.gov.uk>
2016-06-08 18:47:51 +0100

    added page for sessions and 404 errors

--- routes/session.js
+++ routes/session.js
@@ -1,12 +1,23 @@
 (function (routes) {
 
+  var pattern = /^\d*$/;
+
   routes.init = function() {
 
     var express = require('express'),
         router = express.Router();
 
-    router.get('/', function(req, res) {
-      res.send('you hit session /');
+    router.get('/:id', function(req, res) {
+
+      var sessionId = req.params.id;
+
+      if (pattern.test(sessionId) && parseInt(sessionId) < 12 ) {
+        res.render('session/index', {title: 'Your Session', id: sessionId});
+      }
+      else {
+        res.status(400);
+        res.render('errors/404', {title: 'Session Not Found', message: "The session your where looking for cannot be found."});
+      }
     });
 
     return router;
