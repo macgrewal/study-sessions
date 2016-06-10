1fa7910340b4bea771627d08c2de75fefeb85fc1
mac.grewal <mac.grewal@digital.hmrc.gov.uk>
2016-06-11 00:21:33 +0100

    added mongo integration

---
+++ data/seedData.js
@@ -0,0 +1,23 @@
+(function (seedData) {
+
+    seedData.material = [{
+        title: "The Neophyte's Guide to Scala Part 1: Extractors",
+        description: "Explanation of how objects are deconstructed so that they can be used in pattern matching.",
+        type: "blog",
+        url: "http://danielwestheide.com/blog/2012/11/21/the-neophytes-guide-to-scala-part-1-extractors.html",
+        tags: ["Scala", "Extractors", "Pattern Matching"]
+    }, {
+        title: "The Neophyte's Guide to Scala Part 2: Extracting Sequences",
+        description: "Explanation of how sequences are deconstructed so that they can be used in pattern matching.",
+        type: "blog",
+        url: "http://danielwestheide.com/blog/2012/11/28/the-neophytes-guide-to-scala-part-2-extracting-sequences.html",
+        tags: ["Scala", "Extractors", "Pattern Matching", "Sequences"]
+    }, {
+        title: "The Neophyte's Guide to Scala Part 3: Patterns Everywhere",
+        description: "",
+        type: "blog",
+        url: "http://danielwestheide.com/blog/2012/11/21/the-neophytes-guide-to-scala-part-1-extractors.html",
+        tags: ["Scala", "Extractors", "Pattern Matching", "Anonymous Functions", "Partial Functions"]
+    }];
+
+})(module.exports);
