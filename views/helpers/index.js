9ed7ff5f13c882e28c10b6dd8a2da300b44935ab
Alex Fisher <afisher117@googlemail.com>
2016-06-16 20:56:57 +0100

    worked more on initial search page

--- views/helpers/index.js
+++ views/helpers/index.js
@@ -1,10 +1,12 @@
 (function (helpers) {
 
   var input = require('./input'),
+      dropdown = require('./dropdown'),
       textarea = require('./textarea');
 
   helpers.init = function() {
     input.init();
+    dropdown.init();
     textarea.init();
   };
 
