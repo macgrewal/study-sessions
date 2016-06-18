1cde2e2469bcdcd352f676b61465a8e941fefe3b
Alex Fisher <afisher117@googlemail.com>
2016-06-18 17:51:54 +0100

    updated material object and material page

--- routes/material.js
+++ routes/material.js
@@ -10,44 +10,73 @@
         var express = require('express'),
             router = express.Router();
 
-        router.get('/', function(req, res) {
-            res.render('material/add-material', {
-                title: 'Add material',
-                data: {},
-                state: modelHelper.init(req)
-            });
+        // GET: /material
+        router.get('/', function (req, res) {
+          var model = modelHelper.init(req);
+          model.title = 'Add material';
+          res.render('material/add-material', model);
         });
 
-        router.post('/', function(req, res) {
-            var material = {
-                title: req.body.title.trim() || '',
-                description: req.body.description.trim() || '',
-                type: req.body.type.trim() || '',
-                url: req.body.url.trim() || '',
-                tags: req.body.tags.trim() || ''
+        // POST: /material
+        router.post('/', function (req, res) {
+
+          var errors = [],
+            model = {
+              name: req.body.name.trim() || '',
+              description: req.body.description.trim() || '',
+              type: req.body.type.trim() || '',
+              url: req.body.url.trim() || '',
+              tags: req.body.tags.trim() || ''
             };
 
-            var errors = [];
+          if (!model.name) {
+            errors.push({
+              key: 'name',
+              message: 'Enter a name for the material.'
+            });
+          }
+          if (!model.description) {
+            errors.push({
+              key: 'description',
+              message: 'Enter a description for the material.'
+            });
+          }
+          if (!model.type) {
+            errors.push({
+              key: 'type',
+              message: 'Enter a type for the material.'
+            });
+          }
+          if (!model.url) {
+            errors.push({
+              key: 'url',
+              message: 'Enter a url for the material.'
+            });
+          }
+          if (!model.tags) {
+            errors.push({
+              key: 'tags',
+              message: 'Enter at least one tag e.g. Java.'
+            });
+          }
 
-            if (!material.title) errors.push({ key: 'title', message: 'Enter the name of the material.' });
-            if (!material.description) errors.push({ key: 'description', message: 'Enter a description of the material.' });
-            if (!material.type) errors.push({ key: 'type', message: 'Enter the type/format of the material.' });
-            if (!material.url) errors.push({ key: 'url', message: 'Enter the url of the material.' });
-            if (!material.tags) errors.push({ key: 'tags', message: 'Enter at least one tag e.g. the subject.'});
+          model = modelHelper.init(req, model, errors);
 
-            var state = modelHelper.init(req, material, errors);
+          if (model.isValid()) {
+            req.material = {
+              name: model.name,
+              description: model.description,
+              type: model.type, 
+              url: model.url,
+              tags: model.tags.split('\n').join(',')
+            };
 
-            if (!state.hasErrors()) {
-                res.redirect('/material');
-            }
-            else {
-              res.status(400);
-                res.render('material/add-material', {
-                    title: 'Add material',
-                    data: material,
-                    state: state
-                });
-            }
+            res.redirect('material/add-material');
+          } else {
+            res.status(400);
+            model.title = 'Add material';
+            res.render('material/add-material', model);
+          }
         });
 
         return router;
