ab61bab317d19d77aca9f48207fe84764970bacc
mac.grewal <mac.grewal@digital.hmrc.gov.uk>
2016-06-18 16:27:54 +0100

    added filtering on material api

--- data/index.js
+++ data/index.js
@@ -1,14 +1,28 @@
-(function (data) {
+(function(data) {
 
   var seedData = require('./seedData');
   var database = require('./database');
 
-  data.material = function (next) {
-    database.get(function (err, db) {
+
+  data.material = function(options, next) {
+
+    var filter = {};
+
+    if (options.tags) {
+      filter.tags = {
+        $in: options.tags
+      };
+    }
+
+    if (options.type) {
+      filter.type = new RegExp('^' + options.type + '$', 'i');
+    }
+
+    database.get(function(err, db) {
       if (err) {
         next(err);
       } else {
-        db.material.find().toArray(function (err, results) {
+        db.material.find(filter).toArray(function(err, results) {
           if (err) {
             next(err);
           } else {
@@ -20,15 +34,15 @@
   };
 
   function seedDatabase() {
-    database.get(function (err, db) {
+    database.get(function(err, db) {
       if (err) {
         console.log('Failed to seed database: ' + err);
       } else {
-        db.material.count(function (err, count) {
+        db.material.count(function(err, count) {
           if (count === 0) {
             console.log('Seeding collection "material"');
-            seedData.material.forEach(function (item) {
-              db.material.insert(item, function (err) {
+            seedData.material.forEach(function(item) {
+              db.material.insert(item, function(err) {
                 if (err) console.log('Failed to insert material into database');
               });
             });
--- routes/api/material.js
+++ routes/api/material.js
@@ -1,16 +1,23 @@
-(function (routes) {
+(function(routes) {
 
   'use strict';
 
   var data = require('../../data');
 
-  routes.init = function () {
+  routes.init = function() {
 
     var express = require('express'),
       router = express.Router();
 
-    router.get('/', function (req, res) {
-      data.material(function (err, results) {
+    router.get('/', function(req, res) {
+      var tags = req.query.tags || '',
+        type = req.query.type || '',
+        options = {};
+
+      if (tags) options.tags = tags.split(',');
+      if (type) options.type = type;
+
+      data.material(options, function(err, results) {
         if (err) {
           res.err(err);
         } else {
